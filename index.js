a511fab45503b5cd9e7be5f16518267405ac2393
Sonya Bennett <sherwoodsr@hotmail.com>
2018-02-14 22:11:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ function whisper (string) {
   return string.toLowerCase()
 }
 
-function logWhisper (string) {console.log (string.toLowerCase())}
-function logShout (string) {console.log (string.toUpperCase())}
+function logWhisper (string) {
+  console.log (string.toLowerCase())
+}
+function logShout (string) {
+  console.log (string.toUpperCase())
+}
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
    return "I can't hear you!"
   }
-  return `${"I love you, too."}`
+  if (string===string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+if (string==="return `${"I love you, too."}`
   
 }

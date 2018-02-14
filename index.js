9ce5f41bf3a542bafc54732007636aecefb789b3
Sonya Bennett <sherwoodsr@hotmail.com>
2018-02-14 21:52:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function shout (string) {return string.toUpperCase()}
+function shout (string) {
+  return string.toUpperCase()
+  
+}
 function whisper (string) {return string.toLowerCase()}
 function logWhisper (string) {console.log (string.toLowerCase())}
 function logShout (string) {console.log (string.toUpperCase())}
-function sayHiToGrandma(string) {return string toLowerCase (), `${"I can't hear you!"}`}
+function sayHiToGrandma(string) {
+  return string toLowerCase (), `${"I can't hear you!"}`
+  
+}
 function sayHiToGrandma("I love you, Grandma.") {return `${"I love you, too."}`}
